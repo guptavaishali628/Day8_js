21a78857fec218497f53171dfad8b6ca2a16850c
vaishali <guptavaishali628@gmail.com>
2025-05-23 19:17:38 +0530

    first commit

---
+++ boxbgcolorchange.js
@@ -0,0 +1,14 @@
+
+let box=document.querySelector("#box")
+
+let red=()=>{
+    box.style.backgroundColor = "red"
+}
+
+let blue=()=>{
+     box.style.backgroundColor = "blue"
+}
+
+let reset=()=>{
+    box.style.backgroundColor = "white"
+}
--- changecolor.js
+++ changecolor.js
@@ -6,14 +6,19 @@
 let btn1=()=>{
     let mytag1=document.querySelector("#name")
     mytag1.style.color="Red"
+    mytag1.innerHTML="<i>Hello World!</i>"
 }
 
 let btn2=()=>{
     let mytag2=document.querySelector("#name")
     mytag2.style.color="blue"
+    mytag2.innerHTML="<i>Hello World!</i>"
+
 }
 
 let btn3=()=>{
     let mytag3=document.querySelector("#name")
     mytag3.style.color="green"
+    mytag3.innerHTML="<i>Hello World!</i>"
+
 }
