c295acddceda50c5d176318492950c76acf95de3
vaishali <guptavaishali628@gmail.com>
2025-05-24 19:14:02 +0530

    new

---
+++ task1.js
@@ -0,0 +1,4 @@
+let change_img=()=>{
+    let image=document.querySelector("#img")
+    image.src = "./img2.jpg"   // here we will use src to change direct image
+}
---
+++ task2.js
@@ -0,0 +1,8 @@
+let mytag=document.querySelector("#text")
+let hey=()=>{
+    mytag.style.display="none"
+}
+
+let show=()=>{
+    mytag.style.display="block"
+}
