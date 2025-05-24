f55553cb48e9c0e69095ac53a086256c6d525d98
vaishali <guptavaishali628@gmail.com>
2025-05-24 19:32:51 +0530

    new

---
+++ task3.js
@@ -0,0 +1,9 @@
+let okk=()=>{
+    let Text=document.querySelector("#text")
+    if(Text.style.display=="none"){
+       Text.style.display="block"
+    }
+    else{
+        Text.style.display="none"
+    }
+}
