b577f079d22bf7a8531baf132f0fcd264c45370b
vaishali <guptavaishali628@gmail.com>
2025-05-27 00:00:06 +0530

    new

---
+++ onmouseover&leave.js
@@ -0,0 +1,10 @@
+function over(){
+  let over=document.querySelector("#container")
+  over.style.backgroundColor="red"
+}
+
+function leave(){
+  let leave=document.querySelector("#container")
+  leave.style.backgroundColor="blue"
+}
+
